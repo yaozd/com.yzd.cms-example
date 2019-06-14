52eecf2525d85177257de91dfd3b57537ae9cfbc
yaozd <tedijoteamo@163.com>
2019-06-14 14:20:42 +0800

    新闻详情页DEMO

--- html-velocity-demo/src/main/resources/static/content/js/lib/common.js
+++ html-velocity-demo/src/main/resources/static/content/js/lib/common.js
@@ -1,3 +1,81 @@
 /**
  *  工具类
  **/
+(function () {
+    //页面公共对象
+    var common = {
+        apiOrigin: "http://localhost:2380",
+        getContentApiUrl:function(id){
+            var url=common.apiOrigin+"/api/content/"+id+".json";
+            return url;
+        },
+        //获取url参数
+        getUrlParam: function (name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null){
+                var paramValue=unescape(r[2]);
+                return xssEscapeHtml(paramValue);
+            } else return null;
+        },
+        //显示文本内容
+        toText: function (obj) {
+            if (obj == null) {
+                return '';
+            }
+            return obj.toString();
+        },
+        //将时间戳转日期格式
+        formatDateTime: function (inputTime) {
+            var date = new Date(inputTime);
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            m = m < 10 ? ('0' + m) : m;
+            var d = date.getDate();
+            d = d < 10 ? ('0' + d) : d;
+            var h = date.getHours();
+            h = h < 10 ? ('0' + h) : h;
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+            minute = minute < 10 ? ('0' + minute) : minute;
+            second = second < 10 ? ('0' + second) : second;
+            return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+        },
+        isNotEmpty:  function(val){
+            return !this.isEmpty(val);
+        },
+        isEmpty: function(val){
+            if ((val==null || typeof(val)=="undefined")|| (typeof(val)=="string"&&val==""&&val!="undefined")){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    };
+    window.common = common;
+})(jQuery);
+/**
+ * Jquery中避免部分XSS漏洞
+ * 解决办法：将请求参数的字符串中的特殊符号进行转义
+ * @param obj
+ * @returns {string}
+ */
+function xssEscapeHtml(obj) {
+    return(obj)?obj.replace(/</g, '&lt;').replace(/>/g, '&gt;'):"";
+}
+/**
+ * 请求状态
+ * @type {{SUCCESS: number}}
+ */
+var StatusCodeEnum = {
+    SUCCESS:0
+};
+/**
+ * vue全局方法--时间格式化
+ */
+Vue.prototype.formatDate =function (inputTime) {
+     return common.formatDateTime(inputTime);
+};
+Vue.prototype.thumbImg =function (inputImg) {
+    return common.isEmpty(inputImg)?"/":inputImg;
+};
--- html-velocity-demo/src/main/resources/static/script/new/detail.js
+++ html-velocity-demo/src/main/resources/static/script/new/detail.js
@@ -1,3 +1,31 @@
 /**
  *
  */
+var RequestUtil = {
+    initData4ContentFun: function () {
+        var id = common.getUrlParam("id");
+        if (common.isEmpty(id)) {
+            alert("id为空");
+            return;
+        }
+        var url = common.getContentApiUrl(id);
+        axios.get(url)
+            .then(function (response) {
+                //console.log(response.data);
+                var result = response.data;
+                if (result.statusCode != StatusCodeEnum.SUCCESS) {
+                    alert("请求失败");
+                    return;
+                }
+                vm4content.content = result.data;
+            });
+    }
+}
+//VUE 放在代码的最后面
+var vm4content = new Vue({
+    el: "#tpl4content",
+    data: {content: {}},
+    created: function () {
+        RequestUtil.initData4ContentFun();
+    }
+});
